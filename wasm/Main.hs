740eb42b1f0f732f6e6a1d52bf7f37157dc4cf91
Fay Ash <yaoshiu@qq.com>
2026-02-16 23:16:30 +0800

    style: remove unused imports in wasm/Main.hs

--- wasm/Main.hs
+++ wasm/Main.hs
@@ -5,28 +5,22 @@ module Main where
 
 import Foreign.C.String (CString, newCString, peekCString)
 import Foreign.Marshal.Alloc (mallocBytes, free)
-import Foreign.Ptr (Ptr, castPtr)
+import Foreign.Ptr (Ptr)
 import Foreign.StablePtr (StablePtr, deRefStablePtr, freeStablePtr, newStablePtr)
 import Opus (initialEnv, runLine)
 import SExpr (Env)
 import qualified Data.Text as T
 
--- Export memory management functions
 foreign export ccall "opus_malloc" mallocBytes :: Int -> IO (Ptr a)
 foreign export ccall "opus_free" free :: Ptr a -> IO ()
 
--- Create a new environment and return an opaque pointer to it.
--- We use StablePtr to ensure the Haskell GC doesn't move or collect the Env
--- while JavaScript is holding a reference to it.
 foreign export ccall "opus_new_env" opusNewEnv :: IO (StablePtr Env)
 
 opusNewEnv :: IO (StablePtr Env)
 opusNewEnv = initialEnv >>= newStablePtr
 
--- Free the environment pointer when JavaScript is done with it.
 foreign export ccall "opus_free_env" freeStablePtr :: StablePtr Env -> IO ()
 
--- Evaluation function that takes an environment pointer.
 foreign export ccall "opus_eval" opusEval :: StablePtr Env -> CString -> IO CString
 
 opusEval :: StablePtr Env -> CString -> IO CString
